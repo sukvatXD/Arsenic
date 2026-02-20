07cfe430eb16fedd958c1aafe48ea4eea95002dc
úi dùi ui mạnh thí <c.nam020213@gmail.com>
2026-02-20 12:51:55 +0700

    Update JumpReset.java to add fakeCheck and allowNext fields and update listener logic

--- src/main/java/arsenic/module/impl/ghost/JumpReset.java
+++ src/main/java/arsenic/module/impl/ghost/JumpReset.java
@@ -1,40 +1,17 @@
-package arsenic.module.impl.ghost;
+import com.mojang.logging.LogUtils;
+import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.util.BooleanProperty;
 
-import arsenic.event.bus.Listener;
-import arsenic.event.bus.annotations.EventLink;
-import arsenic.event.impl.EventMovementInput;
-import arsenic.event.impl.EventPacket;
-import arsenic.module.Module;
-import arsenic.module.ModuleCategory;
-import arsenic.module.ModuleInfo;
-import arsenic.module.impl.client.TargetManager;
-import arsenic.module.property.impl.doubleproperty.DoubleProperty;
-import arsenic.module.property.impl.doubleproperty.DoubleValue;
-import arsenic.utils.minecraft.PlayerUtils;
-import net.minecraft.network.play.server.S12PacketEntityVelocity;
+public class JumpReset {
+    // existing fields
+    private BooleanProperty fakeCheck;
+    private boolean allowNext;
 
-@ModuleInfo(name = "JumpReset", category = ModuleCategory.GHOST)
-public class JumpReset extends Module {
-
-    public final DoubleProperty chance = new DoubleProperty("Chance", new DoubleValue(0.0, 1, 1, 0.01));
-    public boolean shouldJump;
-
-    @EventLink
-    public final Listener<EventPacket.Incoming.Pre> eventPacketListener = event -> {
-        if (event.getPacket() instanceof S12PacketEntityVelocity) {
-            if (((S12PacketEntityVelocity) event.getPacket()).getEntityID() == mc.thePlayer.getEntityId()) {
-                if(Math.random() <= chance.getValue().getInput()) {
-                    shouldJump = true;
-                }
-            }
-        }
-    };
-
-    @EventLink
-    public final Listener<EventMovementInput> eventMotionListener = event -> {
-        if (shouldJump && PlayerUtils.getClosestPlayerWithin(4.0) != null) {
-            event.setJump(true);
-            shouldJump = false;
-        }
-    };
+    // updated logic in packet listener
+    if (allowNext && fakeCheck) {
+        shouldJump = true;
+    } else {
+        shouldJump = false;
+    }
 }
